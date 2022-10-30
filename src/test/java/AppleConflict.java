cad107260fe13f7247960561fdf5c4c78ed48231
qwer083 <Qwertyiop890>
2022-10-30 17:46:40 +0300

    Initial commit

---
+++ src/test/java/AppleConflict.java
@@ -0,0 +1,9 @@
+public class AppleConflict {
+    public static void main(String []args) {
+        int a = 10;
+        int b = 8;
+        int c = a-b;
+        String s = "осталось яблок у Буратино";
+        System.out.println("осталось яблок у Буратино: " + c);
+    }
+}
