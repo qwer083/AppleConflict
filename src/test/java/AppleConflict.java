05ca74b8773e67f702be3ea14cf633b60ac8e1bb
qwer083 <Qwertyiop890>
2022-10-30 19:56:05 +0300

    final

--- src/test/java/AppleConflict.java
+++ src/test/java/AppleConflict.java
@@ -1,9 +1,13 @@
 public class AppleConflict {
-    public static void main(String []args) {
-        int a = 10;
-        int b = 8;
-        int c = a-b;
-        String s = "осталось яблок у Буратино";
+    public static void main(String[] args) {
+        final int a = 10;
+        final int b = 8;
+        int c = a - b;
+        String s = "было яблок у Буратино";
+        String h = "Карабас отобрал яблоки у Буратино";
+        String j = "осталось яблок у Буратино";
+        System.out.println("было яблок у Буратино: " + a);
+        System.out.println("Карабас отобрал яблоки у Буратино: " + b);
         System.out.println("осталось яблок у Буратино: " + c);
     }
 }
